838cd86a35cb36d096fe69d09b95b285737bd735
Yahya Turunç <116884775+YahyaTurunc@users.noreply.github.com>
2024-12-13 05:15:18 +0300

    update

---
+++ src/pages/Basket.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { Provider } from 'react-redux'
+import store from '../store'
+import BasketDetails from '../components/BasketDetails'
+import BasketDetailSticky from '../components/BasketDetailSticky'
+import Footer from '../components/Footer'
+
+function Basket() {
+  return (<Provider store={store}>
+    <div className="App">
+      <Navbar />
+      <div className=' mx-10 grid grid-cols-6 gap-4 my-10'>
+        <BasketDetails />
+        <div className=''>
+          <BasketDetailSticky />
+        </div>
+      </div>
+      <Footer className={'absolute'} />
+
+    </div>
+  </Provider >
+  )
+}
+
+export default Basket
